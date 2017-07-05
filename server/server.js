570ec60d63af632508dfe40c97440172e2217885
OWNER\Chris <projects@technicacreative.co.jp>
2017-07-05 15:15:15 +0900

    Added 1st API endpoint and resintalled node_modules to fix error

--- server/server.js
+++ server/server.js
@@ -14,11 +14,32 @@ var app = express();
 // now we can send JSON to Express
 app.use(bodyParser.json());
 
-// resource creation
+// this gets the POST from postman
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  // create instance of Mongoose Model
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  // Save the data to MongoDB Todo that was POSTed from /todos url
+  todo.save().then((doc) => {
+    // The Model Todo has the following fields (text, completed, completedAt)
+    // in addition, an _id is auto generated, so the record that is sent back
+    // to POSTMAN (the api) in this case would contain a record that looks like this:
+    // {
+    //     "__v": 0,
+    //     "text": "This is from Postman", // text that was sent into the /todos url as JSON
+    //     "_id": "595c7f2ee6a4d5290cb64495",
+    //     "completedAt": null,
+    //     "completed": false
+    // }
+    // response contains the document from the DB with the data above
+    res.send(doc);
+  }, (err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(3000, () => {
   console.log('Server up on 3000');
-})
+});
