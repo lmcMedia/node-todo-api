cace3e992c3ddaaaaa04d2601e6f6cd2026c47fc
OWNER\Chris <projects@technicacreative.co.jp>
2017-07-12 12:10:49 +0900

    Created Config Vars in Heroku for mLab DB address

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
 mongoose.connect(
-  'mongodb://test-user:testing@ds153652.mlab.com:53652/node-todo-api' || 'mongodb://localhost:27017/TodoApp');
+   process.env.MONGODB_URI
+   || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
