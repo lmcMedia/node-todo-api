5ba2ea63c784ed198e3062a7e0b5ac3c82e2cace
OWNER\Chris <projects@technicacreative.co.jp>
2017-07-12 10:27:36 +0900

    Added test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -31,6 +31,17 @@ app.post('/todos', (req, res) => {
   }, (e) => res.status(400).send(e));
 });
 
+// GET ALL TODOS =====================================================
+app.get('/todos', (req, res) => {
+   // get a list of todos back
+   Todo.find().then((todos) => {
+     // send back the full list (we send back an object since we can add to the object if we want to later)
+     res.send({todos});
+   }, (e) => {
+     res.status(400).send(e);
+   });
+});
+
 // GET WITH ID ========================================================
 // creates id variable in the todos/ url like todos/123456
 // so the API call containing the Todo id will receive back data
@@ -39,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
   // use Mongoose ObjectID to validate id
   if (!ObjectID.isValid(id))
     return res.status(404).send();
-  
+
   // find the Todo by the id in the url
   // res.send sends back either the todo object or an error
   Todo.findById(id).then((todo) => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,21 +3,23 @@ const request = require('supertest');
 
 // ES6 destructuring
 const {app} = require('./../server');
+// can also do like this without destructuring
+//var app = require('./../server').app;
 const {Todo} = require('./../models/todo');
+const {ObjectID} = require('mongodb');
 
+// for testing
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
-// can also do like this without destructuring
-//var app = require('./../server').app;
-
 // lets us run code before each test case
 beforeEach((done) => {
-  // remove all items from Todo database for testing
-  // before any tests are run
+  // remove all items from Todo database for testing before any tests are run
   //Todo.remove({}).then(() => done());
 
   // insert many
@@ -28,9 +30,8 @@ beforeEach((done) => {
 
 describe('Routes', () => {
   describe('HTTP Endpoints', () => {
-
+    // POST ================================================
     describe('POST /todos', () => {
-
       // asyncronous call so make sure to include 'done'
       // TEST FOR CREATING A NEW TODO
       it('should create a new todo', (done) => {
@@ -46,7 +47,6 @@ describe('Routes', () => {
         .end((err, res) => {
           // handle errors from above
           if (err) {
-            // return stops the function execution
             return done(err); // pass the error to done so the test ends and fails
           }
 
@@ -78,6 +78,7 @@ describe('Routes', () => {
       });
     });
 
+    // GET ================================================
     describe('GET /todos', () => {
       it('should get all todos', (done) => {
           // supertest request
@@ -89,6 +90,37 @@ describe('Routes', () => {
           })
           .end(done);
       });
+    });
+
+    // GET /todos/:id  =======================================
+    describe('GET /todos/:id', () => {
+      it('should return todo doc', (done) => {
+        request(app)
+          .get(`/todos/${todos[0]._id.toHexString()}`)
+          .expect(200)
+          .expect((res) => {
+            // called todo in the function so we target the body of todo here
+            expect(res.body.todo.text).toBe(todos[0].text);
+          })
+          .end(done);
+      });
+
+      it('should return a 404 if todo not found', (done) => {
+        // make sure you get a 404 back
+        // real object id calling its toHexString method
+        let id = new ObjectID().toHexString();
+        request(app)
+          .get(`/todos/${id}`)
+          .expect(404)
+          .end(done);
+      });
+
+      it('should return 404 for non-object ids', (done) => {
+        request(app)
+          .get('/todos/123abc')
+          .expect(404)
+          .end(done);
+      })
     });
   });
 });
