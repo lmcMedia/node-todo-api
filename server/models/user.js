910d4bbe13e89b90f0aa4034987af7eef28202d2
OWNER\Chris <projects@technicacreative.co.jp>
2017-07-18 15:12:08 +0900

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -77,7 +77,25 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.token': token,
     'tokens.access': 'auth'
   });
-}
+};
+
+// statics are Model methods
+UserSchema.statics.findByCredentials = function (email,password) {
+  let User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) return Promise.reject();
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user); // inside of Promise so dont need Promise.resolve
+        } else {
+          reject();
+        };
+      });
+    });
+  });
+};
 
 // need access to 'this' so we cannot use ES6 () =>
 UserSchema.pre('save', function (next) {
--- server/server.js
+++ server/server.js
@@ -120,7 +120,22 @@ app.post('/users', (req, res) => {
   }).catch((e) => res.status(400).send(e));
 });
 
+// POST /users/login {email, hashed password} ==========================
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    // create a new token for the user
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 // PRIVATE ROUTE with middleware defined (needs valid token to access)
+// only use authenticate when a token is present
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
